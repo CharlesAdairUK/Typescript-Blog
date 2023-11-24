c24a050311b7af814bafaa8dbe7d89679f352624
Charles Adair <charles.adair93@gmail.com>
2023-11-24 18:34:16 +0000

    Added colour theme toggle tests

--- pages/navigation-bar.ts
+++ pages/navigation-bar.ts
@@ -1,11 +1,35 @@
 import { expect, Locator, Page } from '@playwright/test';
+import { exec } from 'child_process';
 
 export class NavigationBar {
     readonly page: Page;
     readonly homeLink: Locator;
+    readonly colourThemeToggle: Locator;
+    readonly lightTheme: Locator;
+    readonly darkTheme: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.homeLink = page.getByText('My Project');
+        this.colourThemeToggle = page.locator('.nx-relative.nx-grow');
+        this.lightTheme = page.locator('[aria-labelledby="headlessui-listbox-button-:Rlsr6:"] li:first-child');
+        this.darkTheme = page.locator('[aria-labelledby="headlessui-listbox-button-:Rlsr6:"] li:nth-child(2)');
+    }
+
+    
+    async verifyColourThemeToggleVisible() {
+        await expect(this.colourThemeToggle).toBeVisible();
+    }
+
+    async changeColourThemeLight() {
+        await this.colourThemeToggle.click();       
+        await this.lightTheme.click();
+        await expect(this.page.locator('.js-focus-visible.light')).toBeVisible();
+    }
+
+    async changeColourThemeDark() {
+        await this.colourThemeToggle.click();
+        await this.darkTheme.click();
+        await expect(this.page.locator('.js-focus-visible.dark')).toBeVisible();
     }
 }
--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -5,7 +5,7 @@ import { NavigationBar } from '../pages/navigation-bar';
 let homePage: HomePage;
 let navigationBar: NavigationBar;
 const URL = 'https://nextra-docs-template.vercel.app/';
-const pageUrl = /.*aboust/;
+const pageUrl = /.*about/;
 
 test.beforeEach(async ({page}) => {
     await page.goto(URL);
@@ -33,6 +33,21 @@ test.describe('Home Page', () => {
     test('Verify About page button', async ({ page }) => {
         await homePage.clickAboutPageBtn();
         await homePage.assertPageUrl(pageUrl)
+    });
+
+    test('Verify toggle theme button exists', async ({ page }) => {
+        navigationBar = new NavigationBar(page);
+        await navigationBar.verifyColourThemeToggleVisible();
+    });
+
+    test('Verify toggle theme button changes theme to light', async ({ page }) => {
+        navigationBar = new NavigationBar(page);
+        await navigationBar.changeColourThemeLight();
+    });
+
+    test('Verify toggle theme button changes theme to dark', async ({ page }) => {
+        navigationBar = new NavigationBar(page);
+        await navigationBar.changeColourThemeDark();
     });
 
 });
